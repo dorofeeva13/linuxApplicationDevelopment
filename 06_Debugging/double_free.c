09dd737a6e80248305796a7de08cf8cd63a71bd3
dorofeeva97@bk.ru <dorofeeva97@bk.ru>
2020-10-25 17:40:24 +0300

    add code

--- 06_Debugging/double_free.c
+++ 06_Debugging/double_free.c
@@ -4,6 +4,11 @@
 
 int main(int argc, char const *argv[])
 {
-	/* code */
+	int n = 10;
+	char *str = malloc(n);
+	fgets(str, n, stdin);
+	fprintf(stdout, "%s\n", str);
+	free(str);
+	free(str);
 	return 0;
 }
