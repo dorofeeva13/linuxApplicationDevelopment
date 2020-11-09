ee11460a5f909d03a393473d1a7e772ed6bd3d18
dorofeeva97@bk.ru <dorofeeva97@bk.ru>
2020-11-09 20:39:10 +0300

    add files

---
+++ 07_DebuggingTracing/unlink.c
@@ -0,0 +1,15 @@
+#define _GNU_SOURCE
+
+#include <dlfcn.h>
+#include <stdio.h>
+#include <string.h>
+
+typedef int (*unlinkat_type)(int, const char *, int);
+ 
+int unlinkat(int dirfd, const char *pathname, int flags) {
+  unlinkat_type orig_unlinkat = (unlinkat_type)dlsym(RTLD_NEXT, "unlinkat");
+  if(strstr(pathname, "FIX")) {
+    return 0;
+  }
+  return orig_unlinkat(dirfd, pathname,flags);
+}
