4fddc88121d3d5f8087ab20625e246d34051a48a
dorofeeva97@bk.ru <dorofeeva97@bk.ru>
2020-11-15 16:12:51 +0300

    add files

--- 09_I18nL10n/main.c
+++ 09_I18nL10n/main.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
 #include <string.h>
-#include <stdlib.h>
 #include <libintl.h>
 #include <locale.h>
 
+#define _(STRING) gettext(STRING)
+
 int main() {
 	int left = 1, right = 100;
+	setlocale (LC_ALL, "");
+	bindtextdomain ("main", ".");
+  textdomain("main");
 	printf("Make a number from 1 to 100\n");
 	while (left < right) {
 		int middle = (right + left) / 2;
@@ -13,14 +17,14 @@ int main() {
 		printf("Is your number bigger than %d ?\n", middle);
 		printf("If yes - write 'y', else -'n':\n");
 		scanf("%8s", ans);
-		if (strlen(ans) > 1 || !(ans[0] == 'y') || !(ans[0] == 'n')) {
-			printf("Wrong answer, please try again!\n");
-			continue;
-		} else if (ans[0] == 'y') {
+		if (strcmp(ans, _("y"))) {
 			left = middle + 1;
 			continue;
-		} else {
+		} else if (strcmp(ans, _("n"))) {
 			right = middle;
+			continue;
+		} else {
+			printf("Wrong answer, please try again!\n");
 			continue;
 		}
 	}
